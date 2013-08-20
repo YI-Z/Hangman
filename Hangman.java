a258475f42ab8b70a51d22d776d2d9d764a3d735
S <s>
2013-08-19 19:59:10 -0500

    {"type":"TI", "runs":"0"}

--- Hangman.java
+++ Hangman.java
@@ -17,7 +17,7 @@ public class Hangman extends ConsoleProgram {
 	private static final int GUESSES = 8;  //total times for guessing
 	private static int guess_number = GUESSES;  //instant variable for guess numbers
 
-    public void main(void) {
+    public void main(int argc, String argv) {
     	//reset the canvas for drawing
 		canvas.reset();
     	int length = word.length();
